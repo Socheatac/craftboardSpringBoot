02da7e7d73e1f84230ea27f24bc2d7fa1d220af6
Socheata <sotachh@gmail.com>
2020-10-30 16:54:05 -0400

    Initial commit

---
+++ src/test/java/com/craftboard/craftboardSpring/CraftboardSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craftboard.craftboardSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftboardSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
